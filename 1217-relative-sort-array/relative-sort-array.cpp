9c25ecf6153af361d06597656a4fd031dfd43ecd
vishwaaa-kumar <72905310+vishwaaa-kumar@users.noreply.github.com>
2024-06-11 14:32:25 +0530

    Time: 5 ms (34.15%) | Memory: 10.3 MB (24.90%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -10,10 +10,11 @@ public:
             while(mp.find(arr2[i]) != mp.end() && mp[arr2[i]]-- != 0)
                 v.push_back(arr2[i]);
         }
-        sort(arr1.begin(), arr1.end());
-        for (int i = 0; i < arr1.size(); i++) {
-            while(mp.find(arr1[i]) != mp.end() && mp[arr1[i]]-- > 0)
-                v.push_back(arr1[i]);
+        for(auto it : mp)
+        {   
+            int freq=it.second;
+            for(int i=0;i<freq;i++)
+                v.push_back(it.first);
         }
         return v;
     }
